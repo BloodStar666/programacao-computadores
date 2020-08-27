a6fe398c4845ea22ccb27ed7339c8959c224dcf1
alex <alexsanabio@gmail.com>
2020-08-26 21:40:50 -0300

    Exercicio 7, calculando a média

--- Lista1-exe7.js
+++ Lista1-exe7.js
@@ -4,7 +4,7 @@ chefe ()
 function chefe(){
 var vetor = [] // declara o vetor 
 exe7(vetor) // estamos passando o vetor como parametro 
-
+console.log(vetor)
 }
 
 function exe7(vet){ // vet representa o vetor -> o que fizemos com vet, sera feito com o vetor 
@@ -18,5 +18,13 @@ do{
 }    
 while (numero >= 0) 
 
+}
+
+function calculaMedia(vet){
+    var soma = 0 
+    for(var i = 0; i < vet.length; i++){
+        soma = soma + vet[i] 
+    }
+    console.log(`A média é ${soma/vet.length}`)
 }
 
