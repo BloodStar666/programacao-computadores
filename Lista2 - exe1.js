db1c3b6b9cc23c1b0394daa66c3a715da133818a
alex <alexsanabio@gmail.com>
2020-09-08 20:37:32 -0300

    Exercicio 1 completo

--- Lista2 - exe1.js
+++ Lista2 - exe1.js
@@ -8,18 +8,81 @@ let vendas = []
 let opcao
 
 do {
-let opcao = Number(prompt(`Escolha \n [1]. Cadastrar vendedor \n [2]. Cadastrar venda \n [3]. Sair`))
+let opcao = Number(prompt(`Escolha \n [1]. Cadastrar vendedor \n [2]. Cadastrar venda \n [3]. [4].[5]. [6]. Sair`))
 switch(opcao){ 
     case 1: cadastraVendedor(vendedores) 
     break 
     case 2: cadastraVendas(vendas, vendedores) 
     break
-    case 3: console.log(`Tchau`)
+    case 3: consultaVendasFuncionarioMes(vendas)
+    break
+    case 4: consultaVendasFuncionario(vendas) 
+    break
+    case 5: consultaVendedorMes(vendas) 
+    break
+    case 6: consultaMes(vendas)
+    break
+    case 7: console.log(`Tchau`)
     default: console.log(`Tente novamente...`)
 }
 }
-while(opcao != 3)
+while(opcao != 7)
 }
+function consultaMes(vdas){
+    let meses = [0,0,0,0,0,0,0,0,0,0,0,0]
+    for(let i = 0; i < vdas.length; i++){
+        let posicao = vdas[i].mes - 1 // descubro a posição para colocar 
+        meses[posicao] = meses[posicao] + vdas[i].valor // acumula o valor
+    } 
+    // encontraremos o maior valor vendido 
+    let maiorValor = meses[0] // assumo que é janeiro 
+    for(let i = 0; i < meses.length; i++){
+        if(meses[i] > maiorValor){
+            maiorValor = meses[i]
+        }
+    }
+    // encontramos a posição do maior valor vendido 
+    letposicao = meses.indexOf(maiorValor)
+    console.log(`O mês que mais vendeu foi ${posicao+1}`)
+}
+function consultaVendedorMes(vdas){
+    let mes = Number(prompt(`Informe o mês`))
+let maiorVenda = 0 
+let vendedorMaisVendeu = 0
+for(let i = 0; i < vdas.length; i++){
+    if (vdas[i].mes == mes){
+        if(vdas[i].valor > maiorVenda)
+        maiorVenda = vdas[i].valor
+        vendedorMaisVendeu = vdas[i].vendedor
+    }
+} 
+console.log(`O vendedor com mais vendas foi ${vendedorMaisVendeu} e valor ${maiorVenda}`)
+
+function consultasVendasFuncionario(vdas){
+    let vendedor = Number(prompt(`Informe o código do vendedor`))
+    // percorre o vetor de vendas
+    let soma = 0
+    for(let I = 0; i < vdas.length; i++){
+        if(vdas[i].vendedor == vendedor){ // encontrei a venda do vendedor naquele mês
+            soma = soma + vdas[i].valor
+        }
+    }
+    console.log(`A soma das vendas é ${soma}`)
+}
+
+function consultasVendasFuncionarioMes(vdas){
+    let vendedor = Number(prompt(`Informe o código do vendedor`))
+    let mes = Number(prompt(`informe o mês da venda`))
+    // percorre o vetor de vendas
+    for(let I = 0; i < vdas.length; i++){
+        if((vdas[i].vendedor == vendedor) && (vdas[i].mes == mes)){ // enocntrei a venda do vendedor naquele mês
+            console.log(`Valor da venda ${vdas[i].valor}`)
+            return // sai da função
+        }
+    }
+    console.log(`Não existe venda para este funcionario neste mês`)
+}
+
 
 function cadastraVendedor(vended){ // vended representa o vetor vendedores
 let objeto = new Object()
@@ -34,7 +97,6 @@ for(let i = 0; i < vended.length; i++){
 }
 // insere no vended, mas na verdade insere no vetor vendedores 
 vended.push(objeto)
-}
 
 function cadastraVendas(vdas, vdes){ // vdas representa vetor vendas, vdes representa o vetor vendedores 
 
@@ -58,5 +120,5 @@ function cadastraVendas(vdas, vdes){ // vdas representa vetor vendas, vdes repre
     vdas.push(objeto)
     console.log(`Venda nao registrada`)
     return 0 // sai da função 
-    }
+}
 console.log(`Vendedor não existe`)
